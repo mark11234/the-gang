c7b1fa03b403fb07acd0eae84526affebe4a11fc
mark11234 <m_roberts99@icloud.com>
2023-03-21 18:23:58 +0000

    add taskset data

---
+++ src/models/task.ts
@@ -0,0 +1,11 @@
+export type Task = {
+    title: string;
+    difficulty: 
+    {
+        3: number;
+        4: number;
+        5: number;
+    };
+    additionalDetails: string | null;
+    redealCondition: string | null;
+}
